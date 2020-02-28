dcd0ceae2231fa0e5b3fd37207f66c02541eff9d
neo975@gmail.com <mike>
2020-02-28 12:19:40 +0300

    version 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 
 import java.util.List;
@@ -9,16 +10,25 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "D:\\JavaProjects\\artiftest1\\drivers\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\temp2\\mike\\artiftest1\\drivers\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-gpu");
 
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(options);
         Actions actions = new Actions(driver);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+//        priceTest(driver);
+        popupBypass(driver, actions);
+        int k = 44;
+    }
+
+    public static void priceTest(WebDriver driver) {
         driver.get("https://www.ulmart.ru/catalog/cpu");
         List<WebElement> listOfElements = driver.findElements(By.xpath("//div[@id='catalogGoodsBlock']//section"));
 
         for (WebElement rootElement : listOfElements) {
- //           System.out.println(elem.getAttribute("innerHTML"));
+            //           System.out.println(elem.getAttribute("innerHTML"));
             WebElement elementPrice = rootElement.findElement(By.xpath(".//span[@class='b-price__num js-price']"));
             WebElement elementArticular = rootElement.findElement(By.xpath(".//div[@class='b-product__art']/span[@class='num']"));;
             WebElement elementTitle = rootElement.findElement(By.xpath(".//a[@class='must_be_href js-gtm-product-click']"));
@@ -28,15 +38,28 @@ public class Main {
             int price = Integer.valueOf(strPrice);
             System.out.println(strArticular + ", " + price + ", " + elementTitle.getText());
         }
+    }
 
-//Hover test
+    //Hover test
+    public static int popupBypass(WebDriver driver, Actions actions) {
+        driver.get("https://www.ulmart.ru");
         WebElement elementMenu = driver.findElement(By.xpath("//div[@id='b-dropdown-catalog-menu']"));
         actions.moveToElement(elementMenu).perform();
         WebElement elementList = driver.findElement(By.xpath("//ul[@class='b-list b-list_theme_normal b-list_catalog-menu']"));
 
-        WebElement elementAuto = elementList.findElement(By.xpath("//a[@href='/catalog/auto']"));
+//        List<WebElement> elements = elementList.findElements(By.xpath(".//li[@class='b-list__item b-dropdown b-dropdown_toright b-dropdown_catalog-menu dropdown dropdown_theme_normal dropdown_catalog-menu']"));
+        List<WebElement> elements = elementList.findElements(By.xpath("child::*"));
 
-//        driver.quit();
-        int k = 44;
+        for (WebElement parentElement : elements) {
+            actions.moveToElement(parentElement).perform();
+            List<WebElement> childesList = parentElement.findElements(By.xpath(".//li[@class='b-list__item b-list__item_bigger ']"));
+            for (WebElement childElement : childesList) {
+                System.out.println(childElement.findElement(By.xpath(".//a")).getAttribute("href"));
+            }
+        }
+
+        System.out.println(elements.size());
+
+        return elements.size();
     }
 }
